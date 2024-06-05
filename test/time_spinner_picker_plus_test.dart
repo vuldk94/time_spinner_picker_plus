b6b7319500c6dad4c5b0e0f5e45ab1f359ca032c
vuldk <lekhanhvupro@gmail.com>
2024-06-05 23:11:31 +0700

    Add readme

--- test/time_spinner_picker_plus_test.dart
+++ test/time_spinner_picker_plus_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_spinner_picker_plus/time_spinner_picker_plus.dart';
-
 void main() {
-  test('adds one to input values', () {
-  });
+  test('adds one to input values', () {});
 }
